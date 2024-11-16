09c0722ba42a7fc20b765c8631678c09bb7f06a2
leovieira84 <leonardo.vieira@gmail.com>
2024-11-16 18:14:52 -0300

    POST novo produto

--- src/main/java/br/com/leovieira/dscommerce/services/ProductService.java
+++ src/main/java/br/com/leovieira/dscommerce/services/ProductService.java
@@ -1,7 +1,5 @@
 package br.com.leovieira.dscommerce.services;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -28,5 +26,17 @@ public class ProductService {
 		Page<Product> result = repository.findAll(pageable);
 		
 		return result.map(x -> new ProductDTO(x));
+	}
+	
+	@Transactional
+	public ProductDTO insert(ProductDTO dto) {
+		Product entity = new Product();
+		entity.setName(dto.getName());
+		entity.setDescription(dto.getDescription());
+		entity.setPrice(dto.getPrice());
+		entity.setImgUrl(dto.getImgUrl());
+		
+		entity = repository.save(entity);
+		return new ProductDTO(entity);
 	}
 }
