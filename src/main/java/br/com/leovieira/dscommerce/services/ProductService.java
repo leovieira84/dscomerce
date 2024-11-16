fd701b1c9da6ee90f8eabc6a11004724d57ebdd3
leovieira84 <leonardo.vieira@gmail.com>
2024-11-16 17:57:26 -0300

    buscar produtos com paginação

--- src/main/java/br/com/leovieira/dscommerce/services/ProductService.java
+++ src/main/java/br/com/leovieira/dscommerce/services/ProductService.java
@@ -1,8 +1,10 @@
 package br.com.leovieira.dscommerce.services;
 
-import java.util.Optional;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -19,5 +21,12 @@ public class ProductService {
 	public ProductDTO findById(Long id) {
 		Product p = repository.findById(id).get();
 		return new ProductDTO(p);
+	}
+	
+	@Transactional(readOnly = true)
+	public Page<ProductDTO> findAll(Pageable pageable) {
+		Page<Product> result = repository.findAll(pageable);
+		
+		return result.map(x -> new ProductDTO(x));
 	}
 }
